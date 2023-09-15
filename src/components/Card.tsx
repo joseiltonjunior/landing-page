f20c103c215b9a7e85cb6abd034baa0eb7c3c1a5
joseiltonjunior <joseiltonjuniortech@gmail.com>
2023-09-15 11:53:31 -0300

    adjust page

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,16 +5,27 @@ interface CardProps {
   title: string
   description: string
   moreDescription: string
+  urlExample?: string
 }
 
-export function Card({ description, icon, moreDescription, title }: CardProps) {
+export function Card({
+  description,
+  icon,
+  urlExample,
+  moreDescription,
+  title,
+}: CardProps) {
   const [isVisible, setIsVisible] = useState(false)
 
   return (
     <div className="bg-gray-500 rounded-lg p-4 flex flex-col">
       {!isVisible && (
         <>
-          <img src={icon} alt="" className="w-36 h-36 ml-auto mr-auto" />
+          <img
+            src={icon}
+            alt=""
+            className="w-36 h-36 ml-auto mr-auto transform hover:scale-110 transition duration-300"
+          />
           <p className="text-lg font-bold mt-4">{title}</p>
           <p className="mt-2">{description}</p>
         </>
@@ -26,6 +37,17 @@ export function Card({ description, icon, moreDescription, title }: CardProps) {
       >
         {isVisible ? 'Ver menos' : 'Ver mais'}
       </button>
+
+      {urlExample && (
+        <a
+          className="bg-white text-purple-600 rounded-md mt-2 p-2 w-full  hover:bg-white/80 font-bold items-center flex justify-center"
+          href={urlExample}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Exemplo
+        </a>
+      )}
     </div>
   )
 }
